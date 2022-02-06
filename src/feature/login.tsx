4b3b83391a6093e1bef6cb9af2a2bbc8bf467bd2
simjeongho012@gmail.com <simjeongho012@gmail.com>
2022-02-06 19:20:44 +0900

    feat: prettier 적용

--- src/feature/login.tsx
+++ src/feature/login.tsx
@@ -3,41 +3,38 @@ import delay from "../apis/delay";
 import { useDispatch } from "react-redux";
 import { loginRequest, helloSaga } from "../store/slice/user";
 
-const newSagaLogin = createAsyncThunk(
-  "user/logIn",
-  async (data: void, thunkAPI) => {
-    console.log(data);
-    const result = await delay(500, {
-      id: "jeongho",
-      password: "daeun",
-    });
-    return result;
-  }
-);
+const newSagaLogin = createAsyncThunk("user/logIn", async (data: void, thunkAPI) => {
+	console.log(data);
+	const result = await delay(500, {
+		id: "jeongho",
+		password: "daeun",
+	});
+	return result;
+});
 
 export const SagaPracticeLogin = async () => {
-  const dispatch = useDispatch();
-  const result = await delay(1000, {
-    id: "jeongho",
-    password: "daeun",
-  });
-  dispatch(loginRequest());
-  dispatch(helloSaga());
-  console.log(result);
-  return result;
+	const dispatch = useDispatch();
+	const result = await delay(1000, {
+		id: "jeongho",
+		password: "daeun",
+	});
+	dispatch(loginRequest());
+	dispatch(helloSaga());
+	console.log(result);
+	return result;
 };
 
 export const PracticeAsync = async () => {
-  setTimeout(() => {
-    console.log("타임 지나는 중");
-  }, 1000);
+	setTimeout(() => {
+		console.log("타임 지나는 중");
+	}, 1000);
 
-  const result = await delay(1000, {
-    id: "jeongho",
-    password: "daeun",
-  });
+	const result = await delay(1000, {
+		id: "jeongho",
+		password: "daeun",
+	});
 
-  return result;
+	return result;
 };
 
 export default newSagaLogin;
