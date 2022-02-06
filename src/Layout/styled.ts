373c6d5b6cd04adfa324af330a1eebfc06f21cbb
simjeongho012@gmail.com <simjeongho012@gmail.com>
2022-02-06 19:07:30 +0900

    design:  레이아웃 디자인 수정

--- src/Layout/styled.ts
+++ src/Layout/styled.ts
@@ -1,10 +1,18 @@
 import styled from "styled-components";
 
 export const LayoutContainer = styled.section`
+  position: sticky;
+  top: 0;
+  width: 100%;
+  height: 20vh;
+  padding-right: 4%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  /* @media screen and (max-width: 768px) {
+    display: none;
+  } */
 `;
 
 export const LayoutHeader = styled.header`
