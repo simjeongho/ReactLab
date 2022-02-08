71e30c05cdffdb8ec172d91e8cd1487485e65b90
simjeongho012@gmail.com <simjeongho012@gmail.com>
2022-02-08 15:00:48 +0900

    feat: mocking server의 original fetch mock기능

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -15,11 +15,13 @@ export const handlers = [
 		"https://raw.githubusercontent.com/techoi/raw-data-api/main/simple-api.json?id=reactLab",
 		async (req, res, ctx) => {
 			const id = req.url.searchParams.get("id");
-
+			const originalResponse = await ctx.fetch(req);
+			const originalResponseData = await originalResponse.json();
 			return res(
 				ctx.json({
 					data: {
 						people: [
+							...originalResponseData.data.people,
 							{
 								name: "jeongho",
 								age: 135,
@@ -27,22 +29,6 @@ export const handlers = [
 							{
 								name: id,
 								age: 13,
-							},
-							{
-								name: "cindy",
-								age: 15,
-							},
-							{
-								name: "judy",
-								age: 25,
-							},
-							{
-								name: "marry",
-								age: 64,
-							},
-							{
-								name: "tommy",
-								age: 109,
 							},
 						],
 					},
