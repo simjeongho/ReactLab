4cf5ca79c37eae3b7b92041baf37507ea96ec007
simjeongho012@gmail.com <simjeongho012@gmail.com>
2022-02-10 16:48:23 +0900

    feat: 로그인 form custom hook만들기

--- src/hooks/useLoginForm.ts
+++ src/hooks/useLoginForm.ts
@@ -1,19 +1,40 @@
-import { useDispatch } from 'react-redux';
-import AuthService from 'src/auth/auth-service';
+import { loginFailure, loginSuccess } from "@store/slice/user";
+import { useDispatch } from "react-redux";
+import AuthService from "src/auth/auth-service";
 
 const authService = new AuthService();
 const UseLoginForm = (id: string, password: string) => {
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-    const login = async () => {
-        const data = {
-            id: id,
-            password: password,
-        };
+	const login = async () => {
+		const data = {
+			id: id,
+			password: password,
+		};
 
-        try{
-            const{status,
-            data:{}} = await authService.login(data);
-        }
-    }
-}
+		try {
+			const {
+				status,
+				data: { message },
+			} = await authService.login(data);
+			if (status >= 400) {
+				console.log("login fail");
+				dispatch(loginFailure());
+			}
+			if (status === 200) {
+				dispatch(loginSuccess());
+			}
+		} catch (error: any) {
+			console.log(`error!: ${error}`);
+		}
+	};
+
+	const handleSubmitLoginForm = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault(); // 페이지 이동 막음
+		login();
+	};
+
+	return [handleSubmitLoginForm];
+};
+
+export default UseLoginForm;
